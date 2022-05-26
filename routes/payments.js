f9d7a807a09368de746403ef72afbbc1b9ad77a2
archiitsharmaa <archit.sharma@paxcom.net>
2022-05-26 12:12:53 +0530

    dynamic data feilds updated, data refracted

--- routes/payments.js
+++ routes/payments.js
@@ -96,9 +96,11 @@ router.post('/meta-data', verify, async (req, res) => {
         const metaFeildsPaymentType = await Payments.find().distinct('paymentType');
         const metaFeildsPaymentMethod = await Payments.find().distinct('paymentMethod');
         const metaFeildsStatus = await Payments.find().distinct('Status');
+        const metaFeildschannel = await Payments.find().distinct('channel');
 
     
-    return res.json({metaFeildsPaymentType:metaFeildsPaymentType,metaFeildsPaymentMethod:metaFeildsPaymentMethod, metaFeildsStatus :metaFeildsStatus})
+    return res.json({metaFeildsPaymentType:metaFeildsPaymentType,metaFeildsPaymentMethod:metaFeildsPaymentMethod,
+         metaFeildsStatus :metaFeildsStatus, metaFeildschannel: metaFeildschannel})
      }
      catch(err){
        return  res.json({"mssg" : "Invalid refresh token"});
